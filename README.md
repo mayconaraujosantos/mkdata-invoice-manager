d70fe847b4dbd8b527fe8b0474852b8ddf97d1bd
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2023-10-15 16:48:46 -0300

    chore: add structure for project

--- README.md
+++
@@ -1,2 +0,0 @@
-# mkdata-invoice-manager
-The MKData Invoice Manager API provides a streamlined solution for managing invoices
---
+++ src/main/java/com/mkdata/invoicesmanager/InvoicesmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.mkdata.invoicesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicesmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicesmanagerApplication.class, args);
+	}
+
+}
