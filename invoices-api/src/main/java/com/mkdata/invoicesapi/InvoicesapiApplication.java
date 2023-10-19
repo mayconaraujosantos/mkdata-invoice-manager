79c3c6f89680c5d6389b353f271844b6d11ed6a2
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2023-10-19 15:00:10 -0300

    docs: creating module invoices-api

---
+++ invoices-api/src/main/java/com/mkdata/invoicesapi/InvoicesapiApplication.java
@@ -0,0 +1,13 @@
+package com.mkdata.invoicesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicesapiApplication.class, args);
+	}
+
+}
