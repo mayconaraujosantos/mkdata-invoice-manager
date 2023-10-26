64c511e6fb192af5ab11dc19b5db16baff6b3183
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2023-10-26 15:25:31 -0300

    test: add simple test for contextLoads

--- invoices-collector/src/test/java/com/mkdata/invoicescollector/InvoicescollectorApplicationTests.java
+++ invoices-collector/src/test/java/com/mkdata/invoicescollector/InvoicescollectorApplicationTests.java
@@ -3,11 +3,16 @@ package com.mkdata.invoicescollector;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 class InvoicescollectorApplicationTests {
 
 	@Test
 	void contextLoads() {
+		String[] args = {""};
+		Runnable runnable = () -> InvoicescollectorApplication.main(args);
+		assertNotNull(runnable);
 	}
 
 }
